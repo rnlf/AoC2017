858abc311b7b05605cd1ec588436039f2110ac0b
rnlf <rnlf@users.noreply.github.com>
2017-12-01 12:56:05 +0100

    Fixed format string

--- day_01/run.sh
+++ day_01/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-g++ -std=c++17 -O3 -o day_01_1 day_01.cpp
-g++ -std=c++17 -O3 -o day_01_2 -DPART2 day_01.cpp
+CXX=clang++
+
+$CXX -std=c++17 -Ofast -o day_01_1 day_01.cpp
+$CXX -std=c++17 -Ofast -o day_01_2 -DPART2 day_01.cpp
 
 ./day_01_1 input
 ./day_01_2 input
